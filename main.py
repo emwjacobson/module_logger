5199b3d2d26966f628ab1461ea42a5187e91ca01
Emerson Jacobson <emwjacobson@gmail.com>
2024-05-21 07:37:56 -0700

    Added hostname, better logging

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, g
 from datetime import datetime
+import logging
 import psycopg
 
 app = Flask(__name__)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 
 DATABASE = "modules.db"
 
@@ -17,7 +24,7 @@ def get_db():
                 key, value = line.split("=")
                 db_data[key] = value
         db = g._database = psycopg.connect(F"host={db_data['DB_HOST']} dbname={db_data['DB_NAME']} user={db_data['DB_USER']} password={db_data['DB_PASSWORD']}")
-        db.cursor().execute("CREATE TABLE IF NOT EXISTS module_loads (id SERIAL PRIMARY KEY, username TEXT, module TEXT, jid INTEGER, datetime TIMESTAMP);")
+        db.cursor().execute("CREATE TABLE IF NOT EXISTS module_loads (id SERIAL PRIMARY KEY, username TEXT, module TEXT, hostname TEXT, jid INTEGER, datetime TIMESTAMP);")
         db.commit()
     return db
 
@@ -42,15 +49,25 @@ def log():
         jid = -1
     else:
         jid = content['jid']
+    
+    if content['hostname'] == "":
+        app.logger.warning(F"Got invalid hostname from {username} loading {module} at {dt} \n")
+        hostname = ""
+    else:
+        hostname = content['hostname']
 
     data = {
         "username": username,
         "module": module,
         "jid": jid,
-        "dt": dt
+        "dt": dt,
+        "hostname": hostname
     }
 
-    get_cursor().execute("INSERT INTO module_loads (username, module, jid, datetime) VALUES (%(username)s, %(module)s, %(jid)s, %(dt)s)", data)
+    get_cursor().execute("INSERT INTO module_loads (username, module, hostname, jid, datetime) VALUES (%(username)s, %(module)s, %(hostname)s, %(jid)s, %(dt)s)", data)
     get_db().commit()
 
+    app.logger.info(F"Got {username} loading {module} from {hostname} (JID: {jid}) at {dt} \n")
+
     return F"{username} is loading {module} at {dt}\n"
+
